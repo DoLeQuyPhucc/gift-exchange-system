dbf3408e7e509f423beb64beb3dde9645ae9312f
Đỗ Lê Quý Phúc <phucdlq@gmail.com>
2025-01-08 23:45:55 +0700

    add: minor fix

--- src/app/components/create-post/writing-guide.tsx
+++ src/app/components/create-post/writing-guide.tsx
@@ -1,20 +1,23 @@
 interface WritingGuideProps {
-  type: 'title' | 'content'
+  type: "title" | "content";
 }
 
 export const WritingGuide = ({ type }: WritingGuideProps) => {
-  if (type === 'title') {
+  if (type === "title") {
     return (
       <div className="mt-2 p-4 bg-blue-100 text-sm text-gray-700 rounded-lg">
         <h3 className="font-semibold mb-2">Tiêu đề tốt nên có:</h3>
-        <p>Loại sản phẩm + Thương hiệu + Model + Kích thước/Dung tích + Màu sắc + Tình trạng</p>
+        <p>
+          Loại sản phẩm + Thương hiệu + Model + Kích thước/Dung tích + Màu sắc +
+          Tình trạng
+        </p>
         <p className="mt-2">Ví dụ:</p>
         <ul className="list-disc list-inside">
-          <li>- Macbook Pro M1 8GB 256GB 13.3" Retina mới 100%</li>
+          <li>- Macbook Pro M1 8GB 256GB 13.3&quot; Retina mới 100%</li>
           <li>- HP Elitebook 8570p i5/8GB/128GB SSD + 256GB HDD mới 95%</li>
         </ul>
       </div>
-    )
+    );
   }
 
   return (
@@ -26,5 +29,5 @@ export const WritingGuide = ({ type }: WritingGuideProps) => {
         <li>- Sử dụng từ ngữ không phù hợp</li>
       </ul>
     </div>
-  )
-}
+  );
+};
